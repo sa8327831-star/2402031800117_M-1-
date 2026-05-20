58bc2566e35aced6cc55f6e8dfd2b963c923f323
sa8327831-star <sa8327831@gmail.com>
2026-05-20 14:16:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  var name = "shoaib";
+  return (
+    <>
+      <p>Welcome, {name}!</p>
+      <Hello />
+      <img src="React-icon.svg.png" width="100" height="100" />
+    </>
+  );
+}
+ 
---
+++ Hello.jsx
@@ -0,0 +1,7 @@
+function Hello() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+    </>
+  );
+}
